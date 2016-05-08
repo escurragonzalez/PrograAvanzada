7001de9f4ac9779d56f5f25908ce24282486ed4b
lucasmfrancini <lucasm.francini@gmail.com>
2016-05-08 18:05:05 -0300

    Update Test.java

--- TP2B/Source/src/ordenadora/Test.java
+++ TP2B/Source/src/ordenadora/Test.java
@@ -1,113 +1,62 @@
 package ordenadora;
+
 import java.io.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		
-		File archivo;
-		FileReader fr;
+	public static int[] leerArchivo(File file) {
+		int vector[] = null;
+		FileReader fr = null;
 		BufferedReader br;
 
-	try {
-		archivo = new File("..\\Preparacion de la Prueba\\Casos De Prueba\\Ordenadora\\IN-Esperado\\10_Vector350000ElementosAleatorio.in");
-		fr = new FileReader(archivo);
-		br = new BufferedReader(fr);
-		
-		String linea;
-		
-		if((linea = br.readLine())==null){
-			throw new Exception ("Error Lectura Archivo");
+		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String linea;
+
+			if ((linea = br.readLine()) == null) {
+				throw new Exception("Error Lectura Archivo");
+			}
+			int dimension = Integer.parseInt(linea);
+			vector = new int[dimension];
+			for (int i = 0; i < dimension; i++) {
+				vector[i] = Integer.parseInt(br.readLine());
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			try {
+				if (null != file) {
+					try {
+						fr.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
 		}
-		
-		int dimension = Integer.parseInt(linea);
-		int vector[] = new int[dimension];
-		
-		for (int i = 0; i < dimension; i++) {
-			vector[i]=Integer.parseInt(br.readLine());
-		}
-		
-		
-
-
-
-		
-
-		
-		
-		
-		Ordenadora vec;
+		return vector;
+	}
 	
-		vec = new Ordenadora (vector);
-		Calendar tIni;
-		Calendar tFin;
-		long diff;
-		
-//		tIni = new GregorianCalendar();
-//		
-//		vec.Seleccion();
-//		tFin = new GregorianCalendar();
-//		
-//		diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
-//		System.out.println("Tiempo Seleccion: "+diff);
-/////////////////////////////////////////		
-//		 tIni = new GregorianCalendar();
-//	
-//		vec.Shell();
-//		tFin = new GregorianCalendar();
-//		
-//		diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
-//		System.out.println("Tiempo Shell: "+diff);
-/////////////////////////////////////////		
-//		 tIni = new GregorianCalendar();
-//			
-//			vec.Insercion();
-//			tFin = new GregorianCalendar();
-//			
-//			diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
-//			System.out.println("Tiempo Insercion: "+diff);
-/////////////////////////////////////////		
-		 tIni = new GregorianCalendar();
-			
-			vec.Burbuja();
-			tFin = new GregorianCalendar();
-			
-			diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
-			System.out.println("Tiempo Burbuja: "+diff);
-/////////////////////////////////////////
-		
-//			tIni = new GregorianCalendar();
-//			vec.Mezcla(0,vec.vector.length-1);
-//			tFin = new GregorianCalendar();
-//			
-//			diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
-//			System.out.println("Tiempo Mezcla: "+diff);
-/////////////////////////////////////////			
-//			tIni = new GregorianCalendar();
-//			vec.QuickSort(0, (vec.vector.length-1));
-//			tFin = new GregorianCalendar();
-//			
-//			diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
-//			System.out.println("Tiempo Quick Sort: "+diff);
-///////////////////////////////////////////
-			
-			
-			
+	private static void escribirArchivo(String string, int[] vector) {
+		// TODO Auto-generated method stub
 		FileWriter fichero = null;
 		PrintWriter pw = null;
-		
+
 		try {
-			fichero = new FileWriter("outpruebas\\10_Vector350000ElementosAleatorio.out");
+			fichero = new FileWriter(string);
 			pw = new PrintWriter(fichero);
-			pw.println(vec.vector.length);
-			for (int i = 0; i < vec.vector.length; i++) {
-				pw.println(vec.vector[i]);
+			pw.println(vector.length);
+			for (int i = 0; i < vector.length; i++) {
+				pw.println(vector[i]);
 			}
 		} catch (Exception e) {
-			System.out.println("Error de Escritura Archivo de Salida - "+e.getMessage());
+			System.out.println("Error de Escritura Archivo de Salida - " + e.getMessage());
 		} finally {
 			if (null != fichero) {
 				try {
@@ -117,19 +66,44 @@ public class Test {
 				}
 			}
 		}
-		
-		
-		
-		
-		System.out.println("hola");
-		
-				
-	} catch (Exception e) {
-		e.printStackTrace();
 	}
-		
-		
+
+	public static void medirTiempoOrdenamiento(Ordenadora ord) {
+		Calendar tIni;
+		Calendar tFin;
+		long diff;
+		tIni = new GregorianCalendar();
+
+		ord.shell();
+		tFin = new GregorianCalendar();
+
+		diff = tFin.getTimeInMillis() - tIni.getTimeInMillis();
+		System.out.println("Tiempo Shell: " + diff);
 
 	}
+
+	public static void main(String[] args) {
+
+		Ordenadora ord=null;
+		
+		try {
+			File folder = new File("archivos_generados");
+			File[] allFiles = folder.listFiles();
+
+			for (File file : allFiles) {
+				int[] vector = leerArchivo(file);
+				ord = new Ordenadora(vector);
+				System.out.println(file.getName());
+				medirTiempoOrdenamiento(ord);
+								
+				escribirArchivo("archivos_evaluados/"+file.getName(), vector);
+			}	
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+			
+	
+	}
+
 
 }
