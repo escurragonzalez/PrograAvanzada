4e5f9475094e9dfe6e9c471495c986d7a996611a
lucasmfrancini <lucasm.francini@gmail.com>
2016-05-08 18:04:09 -0300

    Update Ordenadora.java

--- TP2B/Source/src/ordenadora/Ordenadora.java
+++ TP2B/Source/src/ordenadora/Ordenadora.java
@@ -1,63 +1,67 @@
 package ordenadora;
 
 public class Ordenadora {
-	int vector[];
+	double vector[];
+	int vectorInt[];
 
 	// Constructor
-	Ordenadora(int V[]) {
-		vector = new int[V.length];
+	Ordenadora(double V[]) {
 		vector = V;
 
 	}
 
-	public void Seleccion() {
+	Ordenadora(int V[]) {
+		vectorInt = V;
+	}
+
+	public void seleccion() {
 		int temp;
 		int posicion_minimo, i, j;
 
-		for (i = 0; i < vector.length - 1; i++) {
+		for (i = 0; i < vectorInt.length - 1; i++) {
 
 			posicion_minimo = i;
-			for (j = i + 1; j < vector.length; j++)
-				if (vector[j] < vector[posicion_minimo])
+			for (j = i + 1; j < vectorInt.length; j++)
+				if (vectorInt[j] < vectorInt[posicion_minimo])
 					posicion_minimo = j;
 
 			if (posicion_minimo != i) {
-				temp = vector[i];
-				vector[i] = vector[posicion_minimo];
-				vector[posicion_minimo] = temp;
+				temp = vectorInt[i];
+				vectorInt[i] = vectorInt[posicion_minimo];
+				vectorInt[posicion_minimo] = temp;
 			}
 
 		}
 
 	}
 
-	public void Insercion() {
+	public void insercion() {
 		int temp;
 		int i, j;
 
-		for (i = 1; i < vector.length; i++) {
-			temp = vector[i];
+		for (i = 1; i < vectorInt.length; i++) {
+			temp = vectorInt[i];
 
-			for (j = i; (j > 0) && temp < vector[j - 1]; j--) {
-				vector[j] = vector[j - 1];
+			for (j = i; (j > 0) && temp < vectorInt[j - 1]; j--) {
+				vectorInt[j] = vectorInt[j - 1];
 			}
 
-			vector[j] = temp;
+			vectorInt[j] = temp;
 
 		}
 
 	}
 
-	public void Burbuja() {
+	public void burbuja() {
 		int i, j;
 		int temp;
 
-		for (i = 0; i < vector.length; i++) {
-			for (j = vector.length - 1; j > i; j--) {
-				if (vector[j] < vector[j - 1]) {
-					temp = vector[j];
-					vector[j] = vector[j - 1];
-					vector[j - 1] = temp;
+		for (i = 0; i < vectorInt.length; i++) {
+			for (j = vectorInt.length - 1; j > i; j--) {
+				if (vectorInt[j] < vectorInt[j - 1]) {
+					temp = vectorInt[j];
+					vectorInt[j] = vectorInt[j - 1];
+					vectorInt[j - 1] = temp;
 				}
 
 			}
@@ -65,46 +69,46 @@ public class Ordenadora {
 
 	}
 
-	public void QuickSort(int izq, int der) {
+	public void quickSort(int izq, int der) {
 
 		int pivote;
 
 		if (izq < der) {
-			pivote = Partir(izq, der);
-			QuickSort(izq, pivote - 1);
-			QuickSort(pivote + 1, der);
+			pivote = partir(izq, der);
+			quickSort(izq, pivote - 1);
+			quickSort(pivote + 1, der);
 		}
 	}
 
-	private int Partir(int pri, int ult) {
-		int pivote = this.vector[pri];
+	private int partir(int pri, int ult) {
+		int pivote = this.vectorInt[pri];
 		int tmp;
 		int izq = pri + 1;
 		int der = ult;
 
 		do {
 
-			while (izq <= der && this.vector[izq] <= pivote)
+			while (izq <= der && this.vectorInt[izq] <= pivote)
 				izq++;
-			while (izq <= der && this.vector[der] > pivote)
+			while (izq <= der && this.vectorInt[der] > pivote)
 				der--;
 
 			if (izq <= der) {
-				tmp = this.vector[izq];
-				this.vector[izq] = this.vector[der];
-				this.vector[der] = tmp;
+				tmp = this.vectorInt[izq];
+				this.vectorInt[izq] = this.vectorInt[der];
+				this.vectorInt[der] = tmp;
 			}
 
 		} while (izq <= der);
 
-		tmp = this.vector[pri];
-		this.vector[pri] = this.vector[der];
-		this.vector[der] = tmp;
+		tmp = this.vectorInt[pri];
+		this.vectorInt[pri] = this.vectorInt[der];
+		this.vectorInt[der] = tmp;
 
 		return der;
 	}
 
-	public void Mezcla(int desde, int elementos) {
+	public void mezcla(int desde, int elementos) {
 		int Mitad;
 		int ParteIzquierda;
 		int ParteDerecha;
@@ -113,71 +117,73 @@ public class Ordenadora {
 			Mitad = elementos / 2;
 			ParteIzquierda = Mitad;
 			ParteDerecha = elementos - Mitad;
-			Mezcla(0, ParteIzquierda);
-			Mezcla(desde + ParteIzquierda, ParteDerecha);
-			OrdenaMezcla(desde, ParteIzquierda, ParteDerecha);
+			mezcla(0, ParteIzquierda);
+			mezcla(desde + ParteIzquierda, ParteDerecha);
+			ordenarMezcla(desde, ParteIzquierda, ParteDerecha);
 
 		}
 
 	}
 
-	private void OrdenaMezcla(int dsd, int Pizq, int Pder) {
+	private void ordenarMezcla(int dsd, int Pizq, int Pder) {
 		int i = 0;
 		int j = 0;
 		int k = 0;
 		int[] tempvec = new int[Pizq + Pder];
 
 		while ((i < Pizq) && (j < Pder)) {
-			if (this.vector[dsd + i] < this.vector[dsd + Pizq + j]) {
-				tempvec[k] = this.vector[dsd + i];
+			if (this.vectorInt[dsd + i] < this.vectorInt[dsd + Pizq + j]) {
+				tempvec[k] = this.vectorInt[dsd + i];
 				i++;
 				k++;
 			} else {
-				tempvec[k] = this.vector[dsd + Pizq + j];
+				tempvec[k] = this.vectorInt[dsd + Pizq + j];
 				j++;
 				k++;
 			}
 
 		}
 		while (i < Pizq) {
-			tempvec[k] = this.vector[dsd + i];
+			tempvec[k] = this.vectorInt[dsd + i];
 			i++;
 			k++;
 		}
 		while (j < Pder) {
-			tempvec[k] = this.vector[dsd + Pizq + j];
+			tempvec[k] = this.vectorInt[dsd + Pizq + j];
 			j++;
 			k++;
 		}
 		for (int l = 0; l < Pizq + Pder; l++) {
-			this.vector[dsd + l] = tempvec[l];
+			this.vectorInt[dsd + l] = tempvec[l];
 
 		}
 
 	}
 
-	public void Shell() {
+	public void shell() {
 
-		int incremento = this.vector.length;
+		int incremento = this.vectorInt.length;
 		int temp = 0;
 		int pivote = 0;
 		do {
 			incremento = incremento / 2;
 
 			for (int i = 0; i < incremento; i++) {
-				for (int j = 0 + i; j < vector.length; j += incremento) {
+
+				for (int j = incremento + i; j < vectorInt.length; j += incremento) {
 					pivote = j;
-					while ((j + incremento < this.vector.length) && this.vector[pivote] > this.vector[j + incremento]) {
-						temp = this.vector[pivote];
-						this.vector[pivote] = this.vector[j + incremento];
-						this.vector[j + incremento] = temp;
-						pivote += incremento;
+					while ((pivote - incremento >= 0) && this.vectorInt[pivote] < this.vectorInt[pivote - incremento]) {
+						temp = this.vectorInt[pivote];
+						this.vectorInt[pivote] = this.vectorInt[pivote - incremento];
+						this.vectorInt[pivote - incremento] = temp;
+						pivote -= incremento;
 					}
 
 				}
 
 			}
 
-		} while (incremento > 1);
+		} while (incremento > 0);
 	}
+
 }
