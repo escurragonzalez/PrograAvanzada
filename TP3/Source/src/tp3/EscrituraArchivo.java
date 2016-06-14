bae2440776ae7fbbb4493856c4c02dfd71829fd9
ealmiron85 <enzojaviergustavoalmiron@gmail.com>
2016-06-13 23:02:07 -0300

    Escritura Formato Salida

--- TP3/Source/src/tp3/EscrituraArchivo.java
+++ TP3/Source/src/tp3/EscrituraArchivo.java
@@ -3,47 +3,63 @@ package tp3;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 
 public class EscrituraArchivo {
 	private Integer cantidadValoresSalida;
 	private Double[] valoresSalida;
-	
+
 	public Integer getCantidadValoresSalida() {
 		return cantidadValoresSalida;
 	}
+
 	public void setCantidadValoresSalida(Integer cantidadValoresSalida) {
 		this.cantidadValoresSalida = cantidadValoresSalida;
-		this.valoresSalida=new Double[this.cantidadValoresSalida];
+		this.valoresSalida = new Double[this.cantidadValoresSalida];
 	}
+
 	public Double[] getValoresSalida() {
 		return valoresSalida;
 	}
-	
-	public void escribirArchivoSalida(String path){
-		FileWriter archivo=null;
-		PrintWriter pw=null;
-		
+
+	public void escribirArchivoSalida(String path) {
+		FileWriter archivo = null;
+		PrintWriter pw = null;
+
 		try {
-			archivo=new FileWriter(path);
-			pw=new PrintWriter(archivo);
-			
+			archivo = new FileWriter(path);
+			pw = new PrintWriter(archivo);
+
 			pw.println(this.cantidadValoresSalida);
 			for (int i = 0; i < valoresSalida.length; i++) {
-				pw.println(this.valoresSalida[i]);
+				pw.println(formatearSalidaArchivo(this.valoresSalida[i]));
 			}
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
-		}
-		finally{
-			if(null!=archivo)
-				try{
+		} finally {
+			if (null != archivo)
+				try {
 					archivo.close();
-				}
-				catch(IOException e){
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 		}
 	}
-	
+
+	private String formatearSalidaArchivo(Double double1) {
+		Long parteEntera = double1.longValue();
+		DecimalFormat df = new DecimalFormat("#.###");
+		DecimalFormat df2 = new DecimalFormat("#");
+		if (double1 == Double.NaN)
+			return double1.toString();
+		if (double1-parteEntera > 0){
+			Long long1 = Math.round(double1*1000);
+			Double double2=((double)long1/1000);
+			return double2.toString();
+		}
+			
+		return df2.format(double1);
+	}
+
 }
