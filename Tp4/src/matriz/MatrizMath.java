bb134ca95125c35fe2be2eb080d5b04127dc75b4
escurragonzalez <escurra.gonzalez@gmail.com>
2016-06-16 23:14:55 -0300

    MAtriz Simetrica

---
+++ Tp4/src/matriz/MatrizMath.java
@@ -0,0 +1,26 @@
+package matriz;
+
+public class MatrizMath{
+	
+	private int dimensionFil;
+	private int dimensionCol;
+	private boolean[][] componentes;
+	
+	public MatrizMath(String path){
+	
+	}
+	
+	public MatrizMath(int dimensionFil,int dimensionCol){
+		this.dimensionFil = dimensionFil;
+		this.dimensionCol = dimensionCol;
+		this.componentes = new boolean[dimensionFil][dimensionCol];
+	}
+
+	public int getOrden() {
+		return dimensionFil;
+	}
+	 public boolean getElemento(int i,int j){
+		 return componentes[i][j];
+	 }
+}
+
---
+++ Tp4/src/matriz/MatrizSimetrica.java
@@ -0,0 +1,45 @@
+package matriz;
+
+public class MatrizSimetrica {
+
+	private boolean[]vector;
+	private int n;//Orden de la Matriz
+	private int t;//tamaño del vector
+	
+	public MatrizSimetrica(MatrizMath matriz,int cantNodo,int canAristas){
+		this.n = matriz.getOrden();
+		t=Math.round((int)(Math.pow(n, 2)-n)/2);
+		vector = new boolean[t];
+		int k=0;//indice en el vector
+		for (int i = 0; i < n; i++) {
+			for(int j=i+1;j<n;j++){
+				vector[k]=matriz.getElemento(i, j);
+				k++;
+			}
+		}
+	}
+	
+	public int getOrdern(){
+		return n;
+	}
+	
+	public boolean getValor(int i,int j){
+		int k;
+		if (i<j){
+			k=(int)(i * this.n + j - ( (i*i) + 3 * i + 2) / 2);
+		}else{
+			k=(int)(j * this.n + i - ( (j*j) + 3 * j + 2) / 2);
+		}
+		return this.vector[k];
+	}
+	
+	public void setValor(int i,int j,boolean valor){
+		int k;
+		if (i<j){
+			k=(int)(i * this.n + j - ( (i*i) + 3 * i + 2) / 2);
+		}else{
+			k=(int)(j * this.n + i - ( (j*j) + 3 * j + 2) / 2);
+		}
+		vector[k]=valor;
+	}
+}
